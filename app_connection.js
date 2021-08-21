8e941228e37acd19baa6546876c10bf8aa109ea8
nukestarz <65008430+nukestarz@users.noreply.github.com>
2021-08-21 09:39:19 +0800

    Add files via upload

---
+++ app_connection.js
@@ -0,0 +1,52 @@
+$(function(){
+    const child_process = require('child_process');
+    const fs = require('fs');
+    const regedit = require('regedit');
+    regedit.setExternalVBSLocation('resources/regedit/vbs')
+    var DIR_FiveM = "";
+    var IPServer = "51.79.162.144:21937";
+    const Shell = require('node-powershell');
+
+    //Get Directory FiveM
+    regedit.list("HKCU\\SOFTWARE\\CitizenFX\\FiveM\\", function(err, result) {
+        $.each(result, function(index, data) {
+            if (data.values["Last Run Location"].value) {
+                DIR_FiveM = data.values["Last Run Location"].value;
+                if (fs.existsSync(DIR_FiveM)) {
+                    $("#btn_start").prop("disabled", false).removeClass("red green").addClass("green").text("Start FiveM.");
+                } else {
+                    $("#btn_start").prop("disabled", true).removeClass("red green").addClass("red").text("You haven't installed FiveM.");
+                }
+                return;
+            }
+        });
+    });
+
+    $("#btn_start").on("click", function(){
+        if (fs.existsSync(DIR_FiveM)) {
+            $("#btn_start").prop("disabled", true);
+            
+            StartFiveM();
+
+            setTimeout(function(){
+                $("#btn_start").prop("disabled", false);
+            }, 2000);
+        }
+    });
+
+    function StartFiveM(){
+        const ps = new Shell({
+            executionPolicy: 'Bypass',
+            noProfile: true
+        });
+        
+        ps.addCommand(`start fivem://connect/${IPServer}`);
+        ps.invoke().then(output => {
+            console.log(output);
+        }).catch(err => {
+            console.log(err);
+        });
+
+    }
+
+});
---
+++ main.js
@@ -0,0 +1,85 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+// const srv = new FiveM.Server('139.99.125.176:30120');
+
+let win, splash;
+let onlineStatusWindow;
+var IMG_DIR = "/img/";
+
+// srv.getPlayers().then(data => console.log(data))
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 1314,
+        height: 748,
+        //titleBarStyle: "hidden",
+        show: false,
+        resizable: false,
+        //frame: false,
+        webPreferences: {
+          nodeIntegration: true
+        },
+         icon: path.join(__dirname, IMG_DIR, "logo.png"),
+        // transparent: true,
+         webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            // nodeIntegration: true,
+            // devTools: true,
+         },
+    });
+    win.setMenu(null)
+    
+    splash = new BrowserWindow({ width: 810, height: 610, transparent: true, frame: false, alwaysOnTop: true });
+    splash.loadURL(`file://${__dirname}/splash.html`);
+    win.setMenuBarVisibility(false)
+    win.loadFile("index.html");
+    //win.webContents.openDevTools();
+}
+
+app.on("ready", () => {
+    // Create the browser window.
+    createWindow();
+    // and load the index.html of the app.
+
+    // Open the DevTools.
+    //win.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    win.on("closed", () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
+
+    win.once("ready-to-show", () => {
+        setTimeout(() => {
+            splash.destroy();
+            win.show();
+        }, 5000);
+    });
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow();
+    }
+});
